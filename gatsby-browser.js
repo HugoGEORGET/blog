5c7db524b31b5f2086bce452db4abb016ebd0395
Hugo Georget <hugo.georget@sword-group.com>
2019-11-21 17:52:23 +0100

    Removed ugly bash prompt

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,6 +4,3 @@ import "typeface-merriweather"
 
 // Markdown code block CSS
 require("prismjs/themes/prism-okaidia.css")
-
-// Add prompt to bash code block
-require("prismjs/plugins/command-line/prism-command-line.css")
--- gatsby-config.js
+++ gatsby-config.js
@@ -40,10 +40,7 @@ module.exports = {
               wrapperStyle: `margin-bottom: 1.0725rem`,
             },
           },
-          {
-            resolve: `gatsby-remark-prismjs`,
-            options: { prompt: { global: true } },
-          },
+          `gatsby-remark-prismjs`,
           `gatsby-remark-copy-linked-files`,
           `gatsby-remark-smartypants`,
         ],
